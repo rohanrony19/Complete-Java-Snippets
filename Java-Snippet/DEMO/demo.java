ff554037bf834ea47d71fdede4656b87196eb728
rohan <rohanprasad1434@gmail.com>
2025-03-26 13:08:30 +0530

    Initial demo

---
+++ Java-Snippet/DEMO/demo.java
@@ -0,0 +1,11 @@
+class demo{
+public static void main(String[] args){
+	System.out.println("My name is Rohan");
+	System.out.println("Am currently pursuing my bachelor degree in Computer Science at Pesitm");
+	System.out.println("Currently doing Internship in X-workz as Software Intern for 4 months at Bangalore");
+	System.out.println("And I have done projects in my college");
+	System.out.println("My short term is to place is to place in a reputed company and My long term goal is to get a higher position in the same company");
+
+
+	}
+}
