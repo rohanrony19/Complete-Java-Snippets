1a6252eb1d1f8b37d912198614eff53f76861ede
Rohan N Prasad <149262357+rohanrony19@users.noreply.github.com>
2025-07-14 22:24:59 +0530

    Delete Java-Snippet directory

--- Java-Snippet/DEMO/job.java
+++
@@ -1,9 +0,0 @@
-class job{
-public static void main(String[] args){
-	System.out.println("My dream job is to become a full stack developer/Data Anlayst");
-	System.out.println("I love building smooth and efficient applications.");
-	System.out.println("At the same time, analyzing data helps me uncover hidden patterns and insights.");
-	System.out.println("Bringing both skills together, I create smart solutions that make a real impact.");	
-	System.out.println("It’s exciting to turn ideas into reality with code and data!");
-	}
-}
