5137b36ca8e0f3fd3ec807ce3c938e7af0a7d6c7
Rohan <rohanprasad1434@gmail.com>
2025-07-15 22:27:07 +0530

    MinElement

---
+++ Complete-Java-Snippets/src/com/rony/Intermediate/Arrays/OneD/MinElement.java
@@ -0,0 +1,14 @@
+package com.rony.Intermediate.Arrays.OneD;
+
+public class MinElement {
+    public static void main(String[] args) {
+        int[] arr = {12,45,7,89,32};
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum = " + min);
+    }
+}
