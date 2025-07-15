2dee56ab773fc14ad70418e2558c658a9be8f755
Rohan <rohanprasad1434@gmail.com>
2025-07-15 22:25:20 +0530

    armstrong number

---
+++ Complete-Java-Snippets/src/com/rony/Intermediate/Interview/Armstrong.java
@@ -0,0 +1,28 @@
+package com.rony.Intermediate.Interview;
+//153 = 1^3 + 5^3 + 3^3
+public class Armstrong {
+    public static void main(String[] args) {
+        int num = 9474;
+        int original = num;
+        int sum = 0;
+
+        int digits = 0;
+        int temp = num;
+        while (temp != 0){
+            digits++;
+            temp = temp/10;
+        }
+        temp = num;
+
+        while (temp != 0){
+            int digit = temp % 10;
+            sum +=  Math.pow(digit,digits);
+            temp /= 10;
+        }
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number");
+        } else {
+            System.out.println(original + " is NOT an Armstrong number");
+        }
+    }
+}
