491b2bd2d16be9f0bf010b019f65593dc5c83b5c
Rohan <rohanprasad1434@gmail.com>
2025-07-15 22:27:58 +0530

    P3

---
+++ Complete-Java-Snippets/src/com/rony/Intermediate/Patterns/P3.java
@@ -0,0 +1,17 @@
+package com.rony.Intermediate.Patterns;
+
+public class P3 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int s = n-i; s > 0; s--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
