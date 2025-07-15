28ddf630b384583669e6c4b013c4ec1a9a0f0e6e
Rohan <rohanprasad1434@gmail.com>
2025-07-15 22:26:00 +0530

    CountFrequency

---
+++ Complete-Java-Snippets/src/com/rony/Intermediate/Arrays/OneD/CountFrequency.java
@@ -0,0 +1,21 @@
+package com.rony.Intermediate.Arrays.OneD;
+
+import java.util.Arrays;
+
+public class CountFrequency {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3,1,4,2};
+        Arrays.sort(arr);
+        int count = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]==arr[i-1]){
+                count++;
+
+        }else {
+                System.out.println(arr[i-1]+ "->" + count + "times");
+                count = 1;
+            }
+        }
+        System.out.println(arr[arr.length-1] +"->"+count + "times");
+    }
+}
