740fffa9e83e98b45fc03ddd1083b82ed6596467
Rohan <rohanprasad1434@gmail.com>
2025-07-17 22:34:02 +0530

    Intermediate

--- Complete-Java-Snippets/src/com/rony/Basic/Interview/Fibonacci.java
+++ Complete-Java-Snippets/src/com/rony/Basic/Interview/Fibonacci.java
@@ -1,4 +1,4 @@
-package com.rony.Intermediate.Interview;
+package com.rony.Basic.Interview;
 
 import java.util.Scanner;
 
---
+++ Complete-Java-Snippets/src/com/rony/Basic/OOPs/Encapsulation/GetCapsule.java
@@ -0,0 +1,9 @@
+package com.rony.Basic.OOPs.Encapsulation;
+
+public class GetCapsule {
+    public static void main(String[] args) {
+        Capsule capsule = new Capsule();
+        System.out.println(capsule.getName()+" my age is "+capsule.getAge()+" and --> "+capsule.getVirginity());
+
+    }
+}
