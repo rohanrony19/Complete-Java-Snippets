663a705f750df0a85cc2b0ca4423f3824684ba00
Rohan <rohanprasad1434@gmail.com>
2025-07-18 19:46:49 +0530

    Intermediate

---
+++ Complete-Java-Snippets/src/com/rony/Intermediate/Exception/NumberFormatExcep.java
@@ -0,0 +1,8 @@
+package com.rony.Intermediate.Exception;
+
+public class NumberFormatExcep {
+    public static void main(String[] args) {
+        String s ="abc";
+        int num = Integer.parseInt(s);
+    }
+}
