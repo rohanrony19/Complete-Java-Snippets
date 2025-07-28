f09e02d9992ccc0823e013d17d0e48e98ed74a59
Rohan <rohanprasad1434@gmail.com>
2025-07-28 21:24:13 +0530

    Intermediate

---
+++ Complete-Java-Snippets/src/com/rony/Basic/OOPs/Inheritance/Multilevel/VehicleRunner.java
@@ -0,0 +1,8 @@
+package com.rony.Basic.OOPs.Inheritance.Multilevel;
+
+public class VehicleRunner {
+    public static void main(String[] args) {
+        Dodge dodge = new Dodge();
+        dodge.drive();
+    }
+}
