c12996b7f35140140f2797f03bbfdbb4b1e7bce9
Rohan <rohanprasad1434@gmail.com>
2025-07-28 21:23:29 +0530

    Intermediate

---
+++ Complete-Java-Snippets/src/com/rony/Advance/Collection/ArrayList/ComparatorUsingLambda.java
@@ -0,0 +1,22 @@
+package com.rony.Advance.Collection.ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ComparatorUsingLambda {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(56);
+        list.add(23);
+        list.add(4);
+        list.add(18);
+        list.sort((a,b)->b-a);
+        System.out.println(list);
+
+        List<String> list1 = Arrays.asList("Papaya","Apple","Pie","Watermelon","Guva");
+        list1.sort((x,y)->x.length()-y.length());
+        System.out.println(list1);
+    }
+}
