4048ad84c579ac067c9ec34a9877a423570aafae
Rohan <rohanprasad1434@gmail.com>
2025-07-28 21:23:57 +0530

    Intermediate

---
+++ Complete-Java-Snippets/src/com/rony/Advance/Collection/Vector/Thread.java
@@ -0,0 +1,35 @@
+package com.rony.Advance.Collection.Vector;
+
+import com.rony.Advance.Collection.ArrayList.Arraylist;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class Thread {
+    public static void main(String[] args) {
+        Vector<Integer> list = new Vector<>();
+//        Thread t1 = new Thread(() -> {
+//            for (int i = 0; i < 1000; i++) {
+//                list.add(i);
+//            }
+//        });
+//
+//        Thread t2 = new Thread(() -> {
+//            for (int i = 0; i < 1000; i++) {
+//                list.add(i);
+//            }
+//        });
+//
+//        t1.start();
+//        t2.start();
+//
+//        try{
+//            t1.join();
+//            t2.join();
+//        }catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
+//        System.out.println("Size of list: " + list.size);//output:2000
+
+    }
+}
