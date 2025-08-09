dbe3d1b5dea86e877a557d077d517f95184ba064
Rohan <rohanprasad1434@gmail.com>
2025-08-08 21:39:21 -0700

    Intermediate

---
+++ Complete-Java-Snippets/src/com/rony/Basic/DataTypes/Static.java
@@ -0,0 +1,23 @@
+package com.rony.Basic.DataTypes;
+
+public class Static {
+    public static void main(String[] args) {
+        Static.age = 10;
+        System.out.println(age);
+        Static.method();
+
+
+    }
+    static int age;
+
+    public Static(int age){
+        this.age = age;
+    }
+
+    static void method(){
+        System.out.println("method");
+    }
+
+
+}
+
