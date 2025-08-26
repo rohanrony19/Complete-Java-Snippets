3b3ddd22352b12350afe9b91655ec7de7a016f3c
Rohan <rohanprasad1434@gmail.com>
2025-08-26 20:05:06 +0530

    Intermediate

--- Complete-Java-Snippets/src/com/rony/Basic/DataTypes/Celsius.java
+++ Complete-Java-Snippets/src/com/rony/Basic/DataTypes/Celsius.java
@@ -10,5 +10,6 @@ public class Celsius {
         float celsius = sc.nextFloat();
         float fahrenheit = (celsius * 9 / 5) + 32;
         System.out.println("Temperature in Fahrenheit : "+fahrenheit);
+
     }
 }
